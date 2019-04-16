85d38da83c066a97ac3faf9e8dfc74d1ffaf1954
jayZhou <503177735@qq.com>
2019-04-16 14:07:29 +0800

    parcel初步打包vue单文件

---
+++ src/button.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+<style lang="scss">
+     .g-button{
+            font-size:var(--font-size);
+            height: var(--button-height);
+            padding: 0px 1em;
+            background:var(--button-bg);
+            border: 1px solid var(--button-border-color);
+            border-radius: 4px; 
+            &:hover{
+                border-color:var(--button-border-color-hover);
+            }
+            &:active{
+                background-color: var(--button-border-bg-active)
+            }
+            &:focus{
+                outline: none;
+                border-color:var(--button-border-color-hover);
+            }
+     }
+</style>
+<script>
+export default {
+
+}
+</script>
+
+
